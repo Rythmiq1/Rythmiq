fbbde1e5a5786208780c1ea6e72a7316fc72cbc0
jatin31195 <jatinsingh31185@gmail.com>
2024-10-29 17:25:16 +0530

    Login-signup form added

--- client/src/pages/Login_signup.jsx
+++ client/src/pages/Login_signup.jsx
@@ -2,7 +2,51 @@ import React from 'react'
 
 function Login_signup() {
   return (
-    <div>Login_signup</div>
+    <div>
+      <div className='sign-up-container'>
+        <form>
+          <h1>Create Account</h1>
+          <input
+          type='text'
+          name='name'
+          placeholder='Enter Your name'
+          />
+
+          <input
+          type='email'
+          name='email'
+          placeholder='Enter Your Email'
+          />
+
+          <input
+          type='password'
+          name='password'
+          placeholder='Enter Your Password'
+          />
+          <button type='submit'>REGISTER</button>
+        </form>
+        <button className='switch-btn Login'>Go to Login</button>
+      </div>
+        <div className='sign-in-container'>
+        <form>
+          <h1>Login</h1>
+
+          <input
+          type='email'
+          name='email'
+          placeholder='Enter Your Email'
+          />
+
+          <input
+          type='password'
+          name='password'
+          placeholder='Enter Your Password'
+          />
+          <button type='submit'>Login</button>
+        </form>
+        <button className='switch-btn'>Go to Register</button>
+        </div>
+    </div>
   )
 }
 
