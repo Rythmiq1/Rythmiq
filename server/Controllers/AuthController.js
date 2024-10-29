e4bf989671d293a9c058e91554dc50d8990704d1
jatin31195 <jatinsingh31185@gmail.com>
2024-10-29 11:53:01 +0530

    controllers added for login request

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -29,9 +29,40 @@ const signup = async (req, res) => {
     }
 }
 
-
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        const errorMsg = 'Authentication failed: email or password is incorrect';
+        if (!user) {
+            return res.status(403).json({ message: errorMsg, success: false });
+        }
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if (!isPasswordCorrect) {
+            return res.status(403).json({ message: errorMsg, success: false });
+        }
+        const jwtToken = jwt.sign(
+            { email: user.email, _id: user._id },
+            process.env.JWT_SECRET,
+            { expiresIn: '24h' }
+        );
+        res.status(200).json({
+            message: "Login successful",
+            success: true,
+            jwtToken,
+            email,
+            name: user.name
+        });
+    } catch (err) {
+        console.error("Login error:", err);
+        res.status(500).json({
+            message: "Internal server error",
+            success: false
+        });
+    }
+}
 
 module.exports = {
     signup,
-    
+    login
 };
