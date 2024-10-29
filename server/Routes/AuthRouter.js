d4dac64a5f659c1c767d709870e7719730fce837
jatin31195 <jatinsingh31185@gmail.com>
2024-10-29 11:53:28 +0530

    middleware added in routes

--- server/Routes/AuthRouter.js
+++ server/Routes/AuthRouter.js
@@ -1,11 +1,9 @@
-const router=require('express').Router();
+const { signup, login } = require('../Controllers/AuthController');
+const { signupValidation, loginValidation } = require('../Middlewares/AuthValidation');
 
-router.post('/login',(req,res)=>{
-  res.send('login success');
-})
+const router = require('express').Router();
 
-router.post('/signup',(req,res)=>{
-  res.send('signup success');
-})
+router.post('/login', loginValidation, login);
+router.post('/signup', signupValidation, signup);
 
-module.exports=router;
+module.exports = router;
