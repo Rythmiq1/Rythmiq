47e75c5bbc86400da06af4c10b85df6f129938c1
jatin31195 <jatinsingh31185@gmail.com>
2024-10-29 20:38:42 +0530

    Implement login functionality with validation and API error handling

--- client/src/pages/Login_signup.jsx
+++ client/src/pages/Login_signup.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { handleError } from '../utils/Error';
 import '../App.css';
 function Login_signup() {
   const [isSignInActive, setIsSignInActive] = useState(true);
@@ -59,6 +60,54 @@ function Login_signup() {
         console.log(result);
     }
 };
+const[loginInfo,setLoginInfo]=useState({
+  email:'',
+  password:''
+})
+const handleLChange=(e)=>{
+  const {name,value}=e.target;
+  console.log(name,value);
+  const copyLoginInfo = {...loginInfo };
+  copyLoginInfo[name] = value;
+  setLoginInfo(copyLoginInfo);
+}
+
+const handleLogin = async (e) => {
+  e.preventDefault();
+  const { name, email, password } = loginInfo;
+  if (!email || !password) {
+      return handleError('All fields are required');
+  }
+  try{
+      const url="http://localhost:8080/auth/login";
+   const response=await fetch(url,{
+      method:'POST',
+      headers:{
+          'Content-Type':'application/json'
+      },
+      body:JSON.stringify(loginInfo)
+  })
+  const result = await response.json();
+          const { success, message,jwtToken,name, error } = result;
+          if (success) {
+              handleSuccess(message);
+              localStorage.setItem('token',jwtToken);
+              localStorage.setItem('loggedInUser',name);
+              setTimeout(() => {
+                  setIsSignInActive(false);
+              }, 1000)
+          } else if (error) {
+              const details = error?.details[0].message;
+              handleError(details);
+          } else if (!success) {
+              handleError(message);
+          }
+          console.log(result);
+}
+  catch(err){
+      handleError(err);
+  }
+};
   return (
 
 
@@ -93,19 +142,22 @@ function Login_signup() {
 
       
       <div className={`form-container sign-in-container ${isSignInActive ? '' : 'hidden'}`}>
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Login</h1>
 
           <input
-          type='email'
+          onChange={handleLChange}
+          type="email" 
           name='email'
-          placeholder='Enter Your Email'
-          />
+          placeholder="Enter your email" 
+          value={loginInfo.email}/>
 
           <input
-          type='password'
+          onChange={handleLChange}
+          type="password" 
           name='password'
-          placeholder='Enter Your Password'
+          placeholder="PASSWORD" 
+          value={loginInfo.password}
           />
           <button type='submit'>Login</button>
         </form>
