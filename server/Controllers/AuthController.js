0ed050a4c6e65995c311b27c0ef1f424794d6cf2
jatin31195 <jatinsingh31185@gmail.com>
2024-10-29 11:52:18 +0530

    controllers added for signup

--- server/Controllers/AuthController.js
+++ server/Controllers/AuthController.js
@@ -1,10 +1,37 @@
-const signup=async(req,res)=>{
-  try {
-    
-  } catch (err) {
-    
-  }
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require("../Models/User");
+
+const signup = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            return res.status(409).json({
+                message: 'User already exists, please login',
+                success: false
+            });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new UserModel({ name, email, password: hashedPassword });
+        await newUser.save();//user saved
+
+        res.status(201).json({
+            message: "Signup successful",
+            success: true
+        });
+    } catch (err) {
+        console.error("Signup error:", err);
+        res.status(500).json({
+            message: "Internal server error",
+            success: false
+        });
+    }
 }
-module.exports={
-  signup
-}
+
+
+
+module.exports = {
+    signup,
+    
+};
