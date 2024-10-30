19e5f95c1e88c229812c9578959df58645718fe4
yashika532 <jainyashika532@gmail.com>
2024-10-30 13:05:24 +0530

    sidebar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,13 +1,34 @@
-import React,{useEffect,useState} from 'react'
+import React, { useEffect, useState } from 'react';
+import logo from "../assets/images/Rhythmiq-bg.ico";
+import IconText from '../components/IconText';
 
 function Home() {
-  const[loggedInUser,setLoggedInUser]=useState('');
-  useEffect(()=>{
-    setLoggedInUser(localStorage.getItem('loggedInUser'))
-  },[])
+  const [loggedInUser, setLoggedInUser] = useState('');
+
+  useEffect(() => {
+    setLoggedInUser(localStorage.getItem('loggedInUser'));
+  }, []);
+
   return (
-    <div>Home  Welcome {loggedInUser}</div>
-  )
+    <div className='h-screen w-screen flex'>
+      {loggedInUser}
+      
+      {/* Left Part */}
+      <div className='h-screen w-1/5 bg-black '>
+        <div className='logoDiv'>
+          <img src={logo} alt="logo" className='w-200 h-100' />
+        </div>
+        <IconText iconName="home" displayText="Home" />
+        <IconText iconName="search" displayText="Search" />
+        <IconText iconName="library_music" displayText="Library" />
+      </div>
+
+      {/* Right Part */}
+      <div className='h-screen w-4/5 bg-gray-100'>
+        {/* Content goes here */}
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
