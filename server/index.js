21bde5f9350e9fe07007e2944c6b1df6d4339063
jatin31195 <jatinsingh31185@gmail.com>
2024-11-01 19:25:28 +0530

    Resolved merge conflict in server/index.js

--- server/index.js
+++ server/index.js
@@ -100,7 +100,11 @@ passport.deserializeUser((user, done) => {
 app.get('/auth/google',passport.authenticate('google',
   {scope: ['profile', 'email']}));
 
+<<<<<<< Updated upstream
   app.get("/auth/google/callback", 
+=======
+app.get("/auth/google/callback", 
+>>>>>>> Stashed changes
     passport.authenticate("google", { failureRedirect: "http://localhost:5173/login" }),
     async (req, res) => {
         if (req.user) {
@@ -117,6 +121,7 @@ app.get('/auth/google',passport.authenticate('google',
     }
 );
 
+<<<<<<< Updated upstream
 
 app.get("/login/success",async(req,res)=>{
     //console.log("reqqqq",req.user)
@@ -129,6 +134,18 @@ app.get("/login/success",async(req,res)=>{
           user:req.user
         }
       )
+=======
+app.get("/login/success", async (req, res) => {
+    if (req.user) {
+        res.status(200).json({
+            message: "User Login",
+            user: req.user
+        });
+    } else {
+        res.status(400).json({
+            message: "User Not Login"
+        });
+>>>>>>> Stashed changes
     }
     else{
       res.status(400).json({
