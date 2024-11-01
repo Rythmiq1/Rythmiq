14a6876deb0cbee25bbc1e7608ccf0db4a8dc3e0
snehaa14 <asneha1412@gmail.com>
2024-11-01 23:09:42 +0530

    added addSong listSong removeSong Controller

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,6 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
-const connectCloudinary = async () => {
+const connectCloudinary = async () =>
+ {
     cloudinary.config({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
