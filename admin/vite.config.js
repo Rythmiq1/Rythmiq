4cc35496875ebc3e14003fb6367a6b0d5665140b
jatin31195 <jatinsingh31185@gmail.com>
2024-11-02 23:01:57 +0530

    fixed port for client and admin

--- admin/vite.config.js
+++ admin/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Change this to your desired fixed port
+    strictPort: true, // Ensure the server fails if the port is already in use
+  },
 })
