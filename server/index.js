3eadf2557b184e3acb00ca5e7b30393413a6372c
jatin31195 <jatinsingh31185@gmail.com>
2025-05-30 14:09:23 +0530

    UPDATE:update cors frontend url

--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000','https://rythmiq-backend.onrender.com'],
+  origin: ['http://localhost:5173', 'http://localhost:3000','https://rythmiq.onrender.com'],
   credentials: true,
 }));
 
@@ -121,7 +121,7 @@ app.use('/artist', ArtistRouter);
 
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:3000'], 
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://rythmiq.onrender.com'], 
     methods: ['GET', 'POST'], 
     allowedHeaders: ['Content-Type'], 
     credentials: true,  
