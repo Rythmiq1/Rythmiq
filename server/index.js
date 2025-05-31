bb69689f7a298074ad06c89d2ca79ce55110bef9
jatin31195 <jatinsingh31185@gmail.com>
2025-05-31 10:49:15 +0530

    fixed google callback url

--- server/index.js
+++ server/index.js
@@ -46,7 +46,7 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback",
+    callbackURL: "https://rythmiq-backend.onrender.com/auth/google/callback",
     scope: ["profile", "email"],
   },
   async (accessToken, refreshToken, profile, done) => {
